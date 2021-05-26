da2776014d7045a320ce0a48a29f1d4a68e93474
mikigv <mikiasgv@gmail.com>
2021-05-26 04:33:10 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="container mx-auto py-6">
+    <header>
+      <div>
+        <span id="logo">Liquid Gold Box</span>
+      </div>
+    </header>
+
+    <div class="content">
+      <FormWizard/>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import FormWizard from './components/FormWizard.vue'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'app',
+  components: {
+    FormWizard
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
